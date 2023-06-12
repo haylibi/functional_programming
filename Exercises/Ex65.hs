cbf5c4c6550218ee6b631a1c263fd6821016f30c
Duarte Pinto <49456938+duartepinto555@users.noreply.github.com>
2023-06-12 17:37:03 +0100

    Delete Exercises directory

--- Exercises/Ex65.hs
+++
@@ -1,8 +0,0 @@
-module Ex65 where
-
-wc = do inp <- getLine
-        let lista = words inp
-        putStrLn ((show (length (lines inp))) ++ "    " ++ (show (length lista)) ++ "    " ++ (show (length inp)))
-		
-main :: IO()
-main = wc
